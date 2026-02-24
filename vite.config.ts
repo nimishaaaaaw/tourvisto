7ca3dfcf4d0b45dd0b4080076ff364771aaa8326
nimishaaaaaw <nimishadadajimajgawali2022@vitbhopal.ac.in>
2026-02-24 05:40:27 +0530

    initial commit: tourvisto travel dashboard

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import {sentryReactRouter, type SentryReactRouterBuildOptions} from "@sentry/react-router";
+
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "js-mastery-yx",
+  project: "travel-agency",
+  authToken: "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"
+};
+
+export default defineConfig(config => {
+  const env = loadEnv(config.mode || 'development', process.cwd(), '');
+  
+  // Expose all env vars to process.env
+  Object.assign(process.env, env);
+
+  return {
+    plugins: [tailwindcss(), tsconfigPaths(), reactRouter(), sentryReactRouter(sentryConfig, config)],
+    sentryConfig,
+    ssr: {
+      noExternal: [/@syncfusion/]
+    }
+  };
+});
